0ca82c29bca73f852193d9531551d6e3342f209f
Sizzard <150135383+Sizzard@users.noreply.github.com>
2024-06-25 10:22:57 +0200

    Update mariadb-script.sh

--- srcs/requirements/mariadb/mariadb-script.sh
+++ srcs/requirements/mariadb/mariadb-script.sh
@@ -3,15 +3,15 @@
 service mariadb start
 
 sleep 3
-mysql -h localhost -u root -p$sql_root_pwd -e "CREATE DATABASE IF NOT EXISTS \`${sql_db}\` ;"
+mysql -h localhost -u root -p$SQL_ROOT_PWD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DB}\` ;"
 sleep 1
-mysql -h localhost -u root -p$sql_root_pwd -e "CREATE USER IF NOT EXISTS \`${sql_usr}\`@'localhost' IDENTIFIED BY \`${sql_pwd}\` ;"
+mysql -h localhost -u root -p$SQL_ROOT_PWD -e "CREATE USER IF NOT EXISTS \`${SQL_USR}\`@'localhost' IDENTIFIED BY \`${SQL_PWD}\` ;"
 sleep 1
-mysql -h localhost -u root -p$sql_root_pwd -e "GRANT ALL PRIVILEGES ON \`${sql_db}\`.* TO \`${sql_usr}\`@'%' IDENTIFIED BY \`${sql_pwd}\` ;"
+mysql -h localhost -u root -p$SQL_ROOT_PWD -e "GRANT ALL PRIVILEGES ON \`${SQL_DB}\`.* TO \`${SQL_USR}\`@'%' IDENTIFIED BY \`${SQL_PWD}\` ;"
 sleep 1
-mysql -h localhost -u root -p$sql_root_pwd -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${sql_root_pwd}\` ;"
+mysql -h localhost -u root -p$SQL_ROOT_PWD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY \`${SQL_ROOT_PWD}\` ;"
 sleep 1
-mysql -h localhost -u root -p$sql_root_pwd -e "FLUSH PRIVILEGES ;"
+mysql -h localhost -u root -p$SQL_ROOT_PWD -e "FLUSH PRIVILEGES ;"
 sleep 1
-mysqladmin -u root -p$sql_root_pwd shutdown
+mysqladmin -u root -p$SQL_ROOT_PWD shutdown
 exec mysqld_safe
